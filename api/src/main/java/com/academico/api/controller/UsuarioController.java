ea4789023e7edb91e77b0615392565f7837aa3d6
Victor GS <victor.vgs2003@gmail.com>
2025-11-02 10:32:44 -0300

    refactor: remoção de imports sem uso

--- api/src/main/java/com/academico/api/controller/UsuarioController.java
+++ api/src/main/java/com/academico/api/controller/UsuarioController.java
@@ -2,13 +2,9 @@ package com.academico.api.controller;
 
 import com.academico.api.dto.UsuarioRequestDTO;
 import com.academico.api.dto.UsuarioResponseDTO;
-import com.academico.api.model.Usuario;
-import com.academico.api.repository.UsuarioRepository;
 import com.academico.api.service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
